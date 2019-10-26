3a4bbcc2d300ccc63710d5f1acdd0cdcda2c86a7
Shaz3b <Shazebhussaini1994@gmail.com>
2019-10-27 00:35:22 +0530

    added model

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
 
 df = pd.read_csv('diabetes.csv')
 
-x = df.loc[:, df.columns != 'Outcome']
+
+X = df.loc[:, df.columns != 'Outcome']
 y = df.loc[:, 'Outcome']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
+
+#Model
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=8))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+#compile the model
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+#Train the model with epochs=200
+model.fit(X_train, y_train, epochs=200)
+
+
+# Results - Accuracy
+scores = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: %.2f%%\n" % (scores[1]*100))
+scores = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy: %.2f%%\n" % (scores[1]*100))
